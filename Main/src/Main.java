4689a38bbbed151362ba47c6dafbbbcd70dc1e58
UBCh <u2b127@gmail.ru>
2021-10-06 21:56:10 +0300

    corrected

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         System.out.println (" ежемесячный платеж = " + resulttwelve + " руб ");
 
         double resulttwentyFour= run.calculate(1_000_000, 2, 9.99);
-        System.out.println (" сумма кредита 1 000 000 руб., срок - 1год. ");
+        System.out.println (" сумма кредита 1 000 000 руб., срок - 2года. ");
         System.out.println (" ежемесячный платеж = " + resulttwentyFour + " руб ");
 
         double resultthirtySix= run.calculate(1_000_000, 3, 9.99);
-        System.out.println (" сумма кредита 1 000 000 руб., срок - 1год. ");
+        System.out.println (" сумма кредита 1 000 000 руб., срок - 3года. ");
         System.out.println (" ежемесячный платеж = " + resultthirtySix + " руб ");
     }
 }
